c49306d9d4ef3cfdf2df5c5cd997f2f65388799f
Johnathan Smith <johnathan.smith@uftwf.org>
2013-06-05 09:45:11 -0400

    got the code working

--- src/test/java/TestHelloWorldWeb.java
+++ src/test/java/TestHelloWorldWeb.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -27,6 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {WebMVCConfig.class})
+@WebAppConfiguration
 public class TestHelloWorldWeb
 {
     @Autowired
