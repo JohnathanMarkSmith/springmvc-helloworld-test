9e6990f820f2acd91d0ed2e63a77c99fdf2a2a9a
Johnathan Smith <johnathan.smith@uftwf.org>
2013-06-05 09:06:07 -0400

    added Test Class

--- src/test/java/TestHelloWorldWeb.java
+++ src/test/java/TestHelloWorldWeb.java
@@ -2,19 +2,30 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
 /**
- * Last changed: $LastChangedDate: 2012-11-14 11:31:35 -0500 (Wed, 14 Nov 2012)
- * $
+ * Date:   6/5/13 / 9:05 AM
+ * Author: Johnathan Mark Smith
+ * Email:  johnathansmith1969@gmail.com
+ * <p/>
+ * Comments:
  *
- * @author $Author$
- * @version $Revision$
+ *    This class is going to be use for testing my Spring MVC Hello World Web App.
+ *
  */
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
 @ContextConfiguration("com.johnathanmsmith.mvc.web.config.WebMVCConfig")
@@ -27,13 +38,16 @@ public class TestHelloWorldWeb
 
     @Before
     public void setup() {
-        this.mockMvc = webAppContextSetup(this.wac).build();
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
     }
 
     @Test
     public void getFoo() throws Exception {
-        this.mockMvc.perform(get("/ask").accept("application/html"))
-                .andExpect(status().isOk());
+        this.mockMvc.perform(get("/ask")
+                .accept(MediaType.TEXT_HTML))
+                .andExpect(status().isOk())
+                .andExpect(view().name("helloworld"));
+
 
     }
 }
